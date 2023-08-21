d6dbc8e67ee05a99e1fc23c58eccf5244b77a63d
Hemant Jain <63767825+jain-hemant@users.noreply.github.com>
2023-08-21 22:18:12 +0530

    Login module successully created

---
+++ login.py
@@ -0,0 +1,7 @@
+print("\t\t\tWelcome To Cap 01 \n\n")
+user_name = input("Enter user name: ")
+password = input("Enter your password: ")
+if user_name == "cap" and password == "12345":
+    print("\t\t\tLogin successfully!")
+else:
+    print("Login failed")
